283146c8282fd2aed1c7176949833d98e989fce9
dhheeraj <dhheeraj@gmail.com>
2021-01-29 11:55:03 +0530

    Removed commented code

--- TilesPoc/src/webparts/megamenu/components/GlobalNav/provider/GlobalNavProvider.ts
+++ TilesPoc/src/webparts/megamenu/components/GlobalNav/provider/GlobalNavProvider.ts
@@ -1,37 +1,13 @@
 import IGlobalNavItem from "../modal/IGlobalNavItem";
 import ISPGlobalNavItem from "../modal/ISPGlobalNavItem";
-//import "@pnp/polyfill-ie11";
-//import { sp } from "@pnp/sp";
+
 import CommonUtility from '../../../../../Utility/common';
 export default class GlobalNavProvider {
   constructor() {
-    // sp.setup({
-    //   sp: {
-    //     headers: {
-    //       Accept: "application/json;odata=verbose"
-    //     },
-    //     baseUrl: `${window.location.protocol}//${window.location.hostname}`
-    //   }
-    // });
+
   }
 
   public async getGlobalNavigation(): Promise<IGlobalNavItem[]> {
-    // const results = await sp.web.lists
-    //   .getByTitle("Global Nav List")
-    //   .items.select(
-    //     "Title",
-    //     "Id",
-    //     "GlobalNavUrl",
-    //     "GlobalNavOpenInNewWindow",
-    //     "GlobalNavParent/Title"
-    //   )
-    //   .top(2000)
-    //   .expand("GlobalNavParent")
-    //   .orderBy("GlobalNavOrder")
-    //   .orderBy("Title")
-    //   .get();
-    // return this.parseGlobalNavigationNodes(results);
-
     let navs = CommonUtility.getNavigations();
     return this.parseGlobalNavigationNodes(navs);
   }
